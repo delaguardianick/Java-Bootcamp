881aa1e4960fdd59dbffb64723b9f8975978f863
delaguardianick <delaguardianick@gmail.com>
2021-10-25 15:12:42 -0400

    upload file

---
+++ JavaBootcampApache/src/main/java/Helloworld.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Gordak
+ */
+public class Helloworld {
+    public static void main(String[] args) {
+        System.out.println("Helloworld");
+    }
+    
+}
