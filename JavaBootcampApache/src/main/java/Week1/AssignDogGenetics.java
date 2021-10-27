c3c9a3e751684c26422f312a848950298a99e771
delaguardianick <delaguardianick@gmail.com>
2021-10-27 13:37:25 -0400

    More uploads

---
+++ JavaBootcampApache/src/main/java/Week1/AssignDogGenetics.java
@@ -0,0 +1,44 @@
+import java.util.Random;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class AssignDogGenetics {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random rand = new Random();
+        ArrayList<Integer> breedRec = new ArrayList<Integer>(numBreeds);
+
+        int numBreeds = 5;
+        int total = 100;
+
+        // For the first breed, choose a random num from 0 to total(100)
+        // For the second, choose a num from 0 to (total -previous breed percentage)
+        // Repeat, this assures they add up to total
+        // Save the percentages in an array for easy access
+        for (int i=0; i < numBreeds-1; i++){
+            int breedPercent = rand.nextInt(total);
+            breedRec.add(breedPercent);
+            total -= breedPercent;
+        }
+        // For the last breed, add the remaining total
+        breedRec.add(total);
+
+        // Take input and display
+        System.out.println("What is your dog's name?");
+        String dogName = sc.nextLine();
+        System.out.println("Well then, I have this highly reliable report on " + dogName + "'s prestigious background right here.");
+        System.out.println(dogName + " is:" + "\n");
+
+        // Access array and print each percent
+        // For formating inside printf, %d is for displaying integers
+        // the double %% is to actually display the % symbol
+        System.out.printf("%d%% St. Bernard\n", breedRec.get(0));
+        System.out.printf("%d%% Chihuahua\n", breedRec.get(1));
+        System.out.printf("%d%% Dramatic RedNosed Asian Pug\n", breedRec.get(2));
+        System.out.printf("%d%% Common Cur\n", breedRec.get(3));
+        System.out.printf("%d%% King Doberman\n", breedRec.get(4));
+        
+
+    }
+}
---
+++ JavaBootcampApache/src/main/java/Week1/wednesday.java
@@ -0,0 +1,18 @@
+/**
+ * Overloading: method belongs to different classes
+ * 
+ * 
+ * Overriding: All methods belong to same class scope
+ *  Same method signature, same num of parameters
+ * 
+ * static: No need to create object to call method
+ *  ok with: Class.method()
+ *  global for whole program
+ * 
+ * non static:
+ *  have to: Class ob1 = new Class();
+ * 
+ * static can NOT call non-static
+ * static can only call static 
+ * nonstatic can call static
+ */
